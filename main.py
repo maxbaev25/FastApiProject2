71912b6f0c73b673f8968e9278671528afeb0396
MaxBaev <mbaev2012@mail.ru>
2026-05-17 11:23:43 +0300

    added logger to main.py

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import uvicorn
 import os
 import platform
 import subprocess
+import logging
 
+# app
 app = FastAPI()
+# logger
+logger = logging.getLogger(name="main")
+logger.setLevel(level=logging.DEBUG)
+# file_handler
+file_handler = logging.FileHandler(
+    filename="main.log", encoding="utf-8")
+logger.addHandler(file_handler)
 
 
 @app.get("/create_file")
 async def create_file_cmd(filename: str = None, content: str = None):
+    logger.debug("GET /create_file")
     # общие проверки
     if filename is None or filename == "." or filename == "..":
         return {"message": "Имя файла не указано!"}
-    if filename.count(".") != 1 or filename.split(".")[1] not in ["txt"]: # тестовое разрешение
+    if filename.count(".") != 1 or filename.split(".")[1] not in ["txt"]:  # тестовое разрешение
         return {"message": "Укажите корректное разрешение файла!"}
     # разделение на переменные для удобства
     name = filename.split(".")[0]
@@ -28,5 +38,7 @@ async def create_file_cmd(filename: str = None, content: str = None):
             subprocess.run(f"echo {content} > {filename}", shell=True)
         else:
             with open(filename, "w") as file:
-                 file.write(content)
+                file.write(content)
     return {"message": f"Файл {filename} успешно создан!"}
+
+uvicorn.run(app, host="0.0.0.0", port=8002)
